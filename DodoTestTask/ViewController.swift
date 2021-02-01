bdac53a9b77e5ff1ee0713c45ee7a6dd08aec4ce
Mikhail Rubanov <m.rubanov@dodopizza.com>
2021-02-01 15:01:21 +0300

    Initial Commit

---
+++ DodoTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DodoTestTask
+//
+//  Created by Mikhail Rubanov on 01.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
